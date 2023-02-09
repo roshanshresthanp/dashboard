cd74dc2841ee68d2ebcb059dce77751c4a2ab880
roshan-sttn <roshan.shrestha@yajtechnologies.com>
2023-02-09 21:37:18 +0545

    change

--- app/Http/Controllers/Admin/SuperController.php
+++ app/Http/Controllers/Admin/SuperController.php
@@ -4,8 +4,80 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class SuperController extends Controller
 {
-    //
+    public $whichModel;
+    public $responseResource;
+
+    public function __construct($whichModel, $responseResource)
+    {
+        $this->whichModel = $whichModel;
+        $this->responseResource = $responseResource;
+    }
+
+    public function getAllFieldNames()
+    {
+        $fillableFields = (new $this->whichModel())->getFillable();
+        return $fillableFields;
+    }
+
+    public function index()
+    {
+        return $this->whichModel::paginate(20);
+    }
+
+    public function createFunction(Request $request)
+    {
+        try {
+            return $this->whichModel::create($request->only($this->getAllFieldNames()));
+        } catch (\Exception $e){
+            return $e;
+        }
+    }
+
+    public function storeFunction(Request $request)
+    {
+        $authUser = Auth::user();
+        $permissionSlug = $this->whichModel::PERMISSIONSLUG;
+//        if (!$authUser->can('create-' . $permissionSlug)) {
+//            throw new AccessDeniedException('unauthorized_access');
+//        }
+        DB::beginTransaction();
+        try {
+            $model = $this->createFunction($request);
+            if (method_exists(new $this->whichModel(), 'afterCreateProcess')) {
+                $model->afterCreateProcess();
+            }
+
+            DB::commit();
+            if ($model instanceof $this->whichModel) {
+                $response = (new $this->responseResource($model))->response()->setStatusCode(200);
+                return $response;
+            } else {
+                return Response::json(array(
+                    'code' => 400,
+                    'message' => $model
+                ), 400);
+            }
+        } catch (\Exception $e) {
+            DB::rollBack();
+            abort(500, $e);
+            return Response::json(array(
+                'code' => 500,
+                'message' => 'Something went wrong'
+            ), 500);
+        }
+    }
+
+    public function updateFunction(Request $request, $id)
+    {
+            $model = $this->whichModel::findOrFail($id);
+            $model->update($request->only($this->getAllFieldNames()));
+            return $model;
+    }
+
+
 }

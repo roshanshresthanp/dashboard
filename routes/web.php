8e9279b7ae06b8221e4f1f093cefc66e79f79784
roshanshresthanp <stha901@gmail.com>
2023-04-11 12:50:17 +0545

    developed sms

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
     return view('admin.layouts.app');
-})->name('dashboard')->middleware('auth');
+})->middleware('auth');
 
 Auth::routes();
 
